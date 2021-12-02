47748fd18afb861d99c53920f191ca3883185f9f
jeremy-then <jeremy.then@iovlabs.org>
2021-12-02 17:01:42 -0400

    Adds length check for index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -24,10 +24,10 @@ describe("bridgeStatus", () => {
     it("should return the bridge status", async () => {
         const bridgeStatusResult = await bridgeStatus(null);
         expect(bridgeStatusResult).to.be.an('object');
-        expect(bridgeStatusResult.activeFederationUtxos).to.be.an('array');
-        expect(bridgeStatusResult.pegoutsWaitingForSignatures).to.be.an('array');
-        expect(bridgeStatusResult.pegoutRequests).to.be.an('array');
-        expect(bridgeStatusResult.pegoutsWaitingForConfirmations).to.be.an('array');
+        expect(bridgeStatusResult.activeFederationUtxos).to.be.an('array').and.to.have.lengthOf(38);
+        expect(bridgeStatusResult.pegoutsWaitingForSignatures).to.be.an('array').and.to.have.lengthOf(0);
+        expect(bridgeStatusResult.pegoutRequests).to.be.an('array').and.to.have.lengthOf(23);
+        expect(bridgeStatusResult.pegoutsWaitingForConfirmations).to.be.an('array').and.to.have.lengthOf(0);
     });
 
 });
