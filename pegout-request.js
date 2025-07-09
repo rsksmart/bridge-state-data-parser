5df02e4fafb21ce630fae827c80d5d3bcbd16656
jeremy-then <jeremy.then@iovlabs.org>
2025-07-09 10:08:48 -0400

    Renames btc destination address to previous

--- pegout-request.js
+++ pegout-request.js
@@ -4,15 +4,15 @@ const RLP = ethUtils.rlp;
 const { BN } = ethUtils;
 
 class PegoutRequest {
-    constructor(btcDestinationAddress, amountInSatoshis, rskTxHash) {
-        this.btcDestinationAddress = btcDestinationAddress;
+    constructor(destinationAddressHash160, amountInSatoshis, rskTxHash) {
+        this.destinationAddressHash160 = destinationAddressHash160;
         this.amountInSatoshis = amountInSatoshis;
         this.rskTxHash = rskTxHash;
     }
 }
 
-function validatePegoutRequestBufferFields(btcDestinationAddressBuffer, amountInSatoshisBuffer, rskTxHashBuffer) {
-    return btcDestinationAddressBuffer.length === 20 && rskTxHashBuffer.length === 32;
+function validatePegoutRequestBufferFields(destinationAddressHash160Buffer, amountInSatoshisBuffer, rskTxHashBuffer) {
+    return destinationAddressHash160Buffer.length === 20 && rskTxHashBuffer.length === 32;
 }
 
 const parseRLPToPegoutRequests = rlp => {
@@ -22,19 +22,21 @@ const parseRLPToPegoutRequests = rlp => {
     if (rlpPegoutRequests.length % 3 !== 0) return pegoutRequests;
 
     for (let i = 0; i < rlpPegoutRequests.length / 3; i++) {
-        const btcDestinationAddressBuffer = rlpPegoutRequests[i * 3];
+        const destinationAddressHash160Buffer = rlpPegoutRequests[i * 3];
         const amountInSatoshisBuffer = rlpPegoutRequests[i * 3 + 1];
         const rskTxHashBuffer = rlpPegoutRequests[i * 3 + 2];
 
-        if (!validatePegoutRequestBufferFields(btcDestinationAddressBuffer, amountInSatoshisBuffer, rskTxHashBuffer)) {
+        if (
+            !validatePegoutRequestBufferFields(destinationAddressHash160Buffer, amountInSatoshisBuffer, rskTxHashBuffer)
+        ) {
             return pegoutRequests;
         }
 
-        const btcDestinationAddress = Buffer.from(btcDestinationAddressBuffer, 'hex').toString('hex');
+        const destinationAddressHash160 = Buffer.from(destinationAddressHash160Buffer, 'hex').toString('hex');
         const amountInSatoshis = new BN(amountInSatoshisBuffer).toString();
         const rskTxHash = Buffer.from(rskTxHashBuffer, 'hex').toString('hex');
 
-        pegoutRequests.push(new PegoutRequest(btcDestinationAddress, amountInSatoshis, rskTxHash));
+        pegoutRequests.push(new PegoutRequest(destinationAddressHash160, amountInSatoshis, rskTxHash));
     }
 
     return pegoutRequests;
--- test/resources/pegout-request-test-data.js
+++ test/resources/pegout-request-test-data.js
@@ -3,27 +3,27 @@ const encodedPegoutRequest =
 
 const decodedPegoutRequest = [
     {
-        btcDestinationAddress: 'cab5925c59a9a413f8d443000abcc5640bdf0675',
+        destinationAddressHash160: 'cab5925c59a9a413f8d443000abcc5640bdf0675',
         amountInSatoshis: '500000',
         rskTxHash: 'f4651c78ab4377713abd54a39b6e34a038cf4b6c4b77698b8a7153776aeecb7d'
     },
     {
-        btcDestinationAddress: '563ef74768a2840eb7069f0cc1d6e3070433459b',
+        destinationAddressHash160: '563ef74768a2840eb7069f0cc1d6e3070433459b',
         amountInSatoshis: '500001',
         rskTxHash: '3b40e9bbe71e1958480ff1604d8053c88c18d183b4647964b65e5ec3e9ba63b0'
     },
     {
-        btcDestinationAddress: '563ef74768a2840eb7069f0cc1d6e3070433459b',
+        destinationAddressHash160: '563ef74768a2840eb7069f0cc1d6e3070433459b',
         amountInSatoshis: '500002',
         rskTxHash: 'b23218c6f8a393c92eef671714217bfebd9f6b30fffd937e277f28b0021e7f87'
     },
     {
-        btcDestinationAddress: '2cad228a9f1e038897d435f3583e741bde3fe8c6',
+        destinationAddressHash160: '2cad228a9f1e038897d435f3583e741bde3fe8c6',
         amountInSatoshis: '500003',
         rskTxHash: 'ef4ea868b27f385e00406484da54c1c74f4d2179cf36e6b99b3c3a92118aabfe'
     },
     {
-        btcDestinationAddress: 'dc6254d5b5c545732e2a65cf0ff075e4070d044f',
+        destinationAddressHash160: 'dc6254d5b5c545732e2a65cf0ff075e4070d044f',
         amountInSatoshis: '500004',
         rskTxHash: 'd4211d935a971d432f337c797b45ca1eeb24957814c2a0fa0b2ae9433dc77b68'
     }
